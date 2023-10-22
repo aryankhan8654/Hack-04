90eaa32f10f6d16edfd1ce26ddbf8a15a9e629b1
aryan <aryankhan8654272@gmail.com>
2023-10-22 02:18:03 -0700

    2nd Commit

--- hack04.c
+++ hack04.c
@@ -8,3 +8,9 @@ int main(int argc, char **argv)
         fprintf(stderr, "Usage: %s <initial_balance> <monthly_contribution> <annual_rate_of_return> <annual_inflation_rate> <years_until_retirement>\n", argv[0]);
         return 1;
     }
+    double initial_balance = atof(argv[1]);
+    double monthly_contribution = atof(argv[2]);
+    double annual_rate_of_return = atof(argv[3]);
+    double annual_inflation_rate = atof(argv[4]);
+    int years_until_retirement = atoi(argv[5]);
+    
